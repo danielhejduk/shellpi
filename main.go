c0bf11df0a738992db6031d4d1a94b1285eb4279
Daniel Hejduk <danielhejduk@disroot.org>
2025-02-13 17:24:00 +0100

    TODO: SQL and timer

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
     "fmt"
     "os"
+    "time"
 
     "github.com/joho/godotenv"
+)
+
+const (
+    CREATE_TABLE = "CREATE TABLE IF NOT EXISTS hourly_readings (date TEXT NOT NULL, time TEXT NOT NULL, l1 INTEGER NOT NULL, l2 INTEGER NOT NULL, l3 INTEGER NOT NULL, total INTEGER NOT NULL, l1_hour INTEGER NOT NULL, l2_hour INTEGER NOT NULL, l3_hour INTEGER NOT NULL, total_hour NOT NULL);"
 )
 
 func main() {
@@ -16,6 +21,19 @@ func main() {
     }
 
     shelly := api{os.Getenv("IP")}
+
+    ticker := time.NewTicker(1 * time.Hour)
+    defer ticker.Stop()
+
+    go func() {
+        for {
+            select {
+                case <-ticker.C:
+                    // TODO
+            }
+        }
+    }()
+
     data := shelly.getdata_request()
     fmt.Printf("L1: %d W\n", data.a_power)
     fmt.Printf("L2: %d W\n", data.b_power) 
